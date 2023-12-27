c5c620a7cb8104f060b9c1d8d16c6b24cc3e9ab1
SahilLodha <sahil.lodha0421@gmail.com>
2023-12-27 19:36:27 +0545

    Updated Code.

--- classes/Employee.py
+++ classes/Employee.py
@@ -1,2 +1,12 @@
-class Employee:
-    pass
+from Person import Person
+
+
+class Employee(Person):
+    def __init__(self, first, last, pay):
+        Person.__init__(self, f'{first} {last}')
+        self.pay = pay
+
+
+    def __str__(self):
+        return f'{Person.__str__(self)} | pay: {self.pay}'
+
