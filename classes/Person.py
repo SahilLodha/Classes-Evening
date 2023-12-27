2ae6304058daaf43eee1153d900e020bf83bbe9b
SahilLodha <sahil.lodha0421@gmail.com>
2023-12-27 18:42:49 +0545

    Person pushed to Repository.

---
+++ classes/Person.py
@@ -0,0 +1,10 @@
+class Person:
+    def __init__(self, name):
+        self.name = name
+    
+    def __str__(self):
+        return f'{self.name}'
+    
+if __name__ == '__main__':
+    p1 = Person("Sahil")
+    print(p1)
